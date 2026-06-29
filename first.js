de392f09309fcd9e02e5863175a44c07c3978f3c
steven <stevenchirwa08@gmail.com>
2026-06-29 11:02:28 +0200

    follow back

---
+++ first.js
@@ -0,0 +1,15 @@
+function steven(num)
+{
+const steven = document.getElementById('calcu');
+steven.value += num;
+}
+function chirwa(){
+const variabo = document.getElementById('calcu');
+
+variabo.value ='';
+}
+function duncan()
+{
+const variabo = document.getElementById('calcu');
+variabo.value = eval(variabo.value);
+}
